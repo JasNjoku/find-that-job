18ec0716686abc10e7495ab9a794f327a2497d0a
Jason Njoku <JasNjoku@gmail.com>
2023-03-27 14:09:31 +0100

    added error handling to return empty array if no jobs found

--- root/server/routes/jobs.js
+++ root/server/routes/jobs.js
@@ -15,6 +15,7 @@ async function start() {
         const page = await browser.newPage();
         let jobs = []
 
+        //http headers
         await page.setExtraHTTPHeaders({
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
             'upgrade-insecure-requests': '1',
@@ -23,69 +24,73 @@ async function start() {
             'accept-language': 'en-US,en;q=0.9,en;q=0.8'
         })
 
-        console.log("Searching....")
-
         async function getIndeedJobs() {
             await page.goto(`https://ie.indeed.com/jobs?q=${query}&l=${location}`)
-            await page.waitForSelector('nav > div > .css-ns2mzi')
-            await page.screenshot({ path: 'Test.png', fullPage: true }).then(console.log('Screenshot taken!'))
+            await page.waitForSelector('body')
 
-           return await page.evaluate(() => {
-                return Array.from(document.querySelectorAll('.resultContent')).map(job => {
-                    return {
-                        website: 'Indeed',
-                        jobTitle: job.querySelector('.jobTitle > a > span').textContent,
-                        jobCompany: job.querySelector('.companyName').textContent,
-                        jobLocation: job.querySelector('.companyLocation').textContent
-                    }
+            try {
+                return await page.evaluate(() => {
+                    return Array.from(document.querySelectorAll('.resultContent')).map(job => {
+                        return {
+                            website: 'Indeed',
+                            jobTitle: job.querySelector('.jobTitle > a > span').textContent.trim(),
+                            jobCompany: job.querySelector('.companyName').textContent.trim(),
+                            jobLocation: job.querySelector('.companyLocation').textContent.trim()
+                        }
+                    })
                 })
-            })
+            } catch (err) {
+                console.error(`Error getting Indeed jobs: ${err}`)
+                return []
+            }
         }
 
         async function getLinkedInJobs() {
             await page.goto(`https://www.linkedin.com/jobs/search?keywords=${query}&location=${location}`)
-            await page.waitForSelector('.jobs-search__results-list')
-            
-            return await page.evaluate(() => {
-                return Array.from(document.querySelectorAll('.base-search-card__info')).map(job => {
-                    
-                    return {
-                        website: 'LinkedIn',
-                        jobTitle: job.querySelector('.base-search-card__title').textContent.trim(),
-                        jobCompany: job.querySelector('.base-search-card__subtitle > a').textContent.trim(),
-                        jobLocation: job.querySelector('.base-search-card__metadata > span').textContent.trim()
-                    }
+            await page.waitForSelector('body')
+
+            try {
+                return await page.evaluate(() => {
+                    return Array.from(document.querySelectorAll('.base-search-card__info')).map(job => {
+                        return {
+                            website: 'LinkedIn',
+                            jobTitle: job.querySelector('.base-search-card__title').textContent.trim(),
+                            jobCompany: job.querySelector('.base-search-card__subtitle > a').textContent.trim(),
+                            jobLocation: job.querySelector('.base-search-card__metadata > span').textContent.trim()
+                        }
+                    })
                 })
-            })
+            } catch (err) {
+                console.error(`Error getting LinkedIn jobs: ${err.message}`)
+                return []
+            }
         }
 
         async function getIrishJobs() {
-            await page.goto(`https://www.irishjobs.ie/ShowResults.aspx?Keywords=${query}&Location=0`)
-            await page.waitForSelector('.two-thirds');
-            await page.screenshot({ path: 'Test.png', fullPage: true }).then(console.log('Screenshot taken!'))
-        
-            return await page.evaluate(() => {
-                return Array.from(document.querySelectorAll('div.job-result')).map(job => {
-           
-                    return {
-                        website: 'IrishJobs',
-                        jobTitle: job.querySelector('div.job-result-title > h2 > a').textContent.trim(),
-                        jobCompany: job.querySelector('div.job-result-title > h3 > a').textContent.trim(),
-                        jobLocation: job.querySelector('div.job-result-overview > ul > .location > a').textContent.trim()
-                    }
-                })
-            })
-
+            try {
+                await page.goto(`https://www.irishjobs.ie/ShowResults.aspx?Keywords=${query}&Location=0`);
+                await page.waitForSelector('.two-thirds');
+                return await page.evaluate(() => {
+                    return Array.from(document.querySelectorAll('div.job-result')).map(job => {
+                        return {
+                            website: 'IrishJobs',
+                            jobTitle: job.querySelector('div.job-result-title > h2 > a').textContent.trim(),
+                            jobCompany: job.querySelector('div.job-result-title > h3 > a').textContent.trim(),
+                            jobLocation: job.querySelector('div.job-result-overview > ul > .location > a').textContent.trim()
+                        }
+                    });
+                });
+            } catch (error) {
+                console.error('An error occurred while getting IrishJobs:', error);
+                return [];
+            }
         }
+        jobs = [...jobs, ...await getIndeedJobs(),...await getIrishJobs(), ...await getLinkedInJobs()]
 
-
-       jobs = [...jobs, ...await getIrishJobs(), ...await getLinkedInJobs(), ...await getIndeedJobs()]
-        
 
 
         res.json(jobs)
         await browser.close().then(console.log('Connection Closed'))
-        // res.send("Please Check Test Photo In your Folder")
     })
 
 }
@@ -93,3 +98,7 @@ async function start() {
 start();
 
 module.exports = router;
+
+
+
+
