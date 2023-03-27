909a1bd912d49155ba2674b92206b87c2ec5f054
Jason Njoku <JasNjoku@gmail.com>
2023-03-27 08:45:51 +0100

    removed test route

--- root/server/index.js
+++ root/server/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 4000
-var cors = require('cors')
-//routes
-const locationRoute = require('./routes/locations')
-const jobsRoute = require('./routes/jobs')
+const cors = require('cors')
 
+const jobsRoute = require('./routes/jobs')
 
 app.use(cors())
 
@@ -13,9 +11,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use('/locations', locationRoute);
 app.use('/jobs', jobsRoute)
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
